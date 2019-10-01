829ca3357ec23772b849c0c4672a919c51c95567
brickmack <brickmack@gmail.com>
2019-10-01 10:14:29 -0400

    obsolecence fixes for new cakephp

--- config/routes.php
+++ config/routes.php
@@ -75,9 +75,3 @@
 	 */
 	$routes->fallbacks(DashedRoute::class);
     });
-
-    /**
-     * Load all plugin routes. See the Plugin documentation on
-     * how to customize the loading of plugin routes.
-     */
-    Plugin::routes();
